cdd832510a43216b8315035272872dff17c17aad
akashkumar-404 <akashkumar404@gmail.com>
2022-09-10 17:58:21 +0530

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://pbs.twimg.com/profile_images/1273636051047690242/_bztELuK_400x400.jpg' />
-      <h1>Akash Kumar Gupta</h1>
-      <h1>7209777893</h1>
-      <h1>ak</h1>
-      <h1>12345</h1>
+        <img src="https://pbs.twimg.com/profile_images/1273636051047690242/_bztELuK_400x400.jpg" />
+        <h1>Akash Kumar Gupta</h1>
+        <h1>7209777893</h1>
+        <h1>ak</h1>
+        <h1>12345</h1>
+        <h1>12345</h1>
       </header>
     </div>
   );

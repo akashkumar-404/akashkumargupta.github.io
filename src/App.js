47a381626f4b23c8cdaa5fd404371eac8d7be9cc
akashkumar-404 <akashkumar404@gmail.com>
2022-09-10 17:26:59 +0530

    part3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src='https://pbs.twimg.com/profile_images/1273636051047690242/_bztELuK_400x400.jpg' />
       <h1>Akash Kumar Gupta</h1>
       <h1>7209777893</h1>
+      <h1>ak</h1>
       </header>
     </div>
   );
